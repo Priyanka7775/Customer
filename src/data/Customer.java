a8a08dd0709128a0e3b0691df6db8ed78773c827
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-16 14:24:24 +0530

    updated

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -1,4 +1,50 @@
 package data;
 
 public class Customer {
+    String name;
+    String address;
+    int mobileNo;
+    String email;
+    boolean premiumMembership;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPremiumMembership(boolean premiumMembership) {
+        this.premiumMembership = premiumMembership;
+    }
+    public boolean isPremiumMembership() {
+        return premiumMembership;
+    }
+
+
 }
--- src/main/CustomerImpl.java
+++ src/main/CustomerImpl.java
@@ -1,4 +1,37 @@
 package main;
 
+import data.Customer;
+
 public class CustomerImpl {
+    public static void main(String[] args) {
+        Customer obj1=new Customer();
+        obj1.setName("Ramesh");
+        obj1.setEmail("ramesh123@gmail.com");
+        obj1.setAddress("Mumbai");
+        obj1.setMobileNo(123456789);
+
+        System.out.println("obj1.getName() = " + obj1.getName());
+        System.out.println("obj1.getAddress() = " + obj1.getAddress());
+        System.out.println("obj1.getMobileNo() = " + obj1.getMobileNo());
+        System.out.println("obj1.getEmail() = " + obj1.getEmail());
+        System.out.println("obj1.isPremiumMembership() = " + obj1.isPremiumMembership());
+
+
+        System.out.println();
+
+        Customer obj2=new Customer();
+        obj2.setName("Suresh");
+        obj2.setEmail("suresh123@gmail.com");
+        obj2.setAddress("Delhi");
+        obj2.setMobileNo(987654321);
+
+        System.out.println("obj2.getName() = " + obj2.getName());
+        System.out.println("obj2.getAddress() = " + obj2.getAddress());
+        System.out.println("obj2.getMobileNo() = " + obj2.getMobileNo());
+        System.out.println("obj2.getEmail() = " + obj2.getEmail());
+        System.out.println("obj2.isPremiumMembership() = " + obj2.isPremiumMembership());
+
+
+
+    }
 }
